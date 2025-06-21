12551ff83369fadc0df9f8f17f62238589d6f925
Artem <plzv92@gmail.com>
2025-06-21 18:46:08 +0700

    fix: styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <div className="min-h-screen max-w-[575px] mx-auto h-screen flex flex-col items-center justify- pb-10">
       <h1>hypnosisflow labs</h1>
-      <div className="w-full max-w-[1440px] relative h-full flex flex-col">
+      <div className="w-full max-w-[575px] relative h-full flex flex-col">
         {/* <div className="flex-1 flex flex-col items-center justify-center min-h-[500px] gap-8">
           <Dashboard />
           <CoinsTable />
         </div> */}
-        <div className="absolute translate-x-1/2">
+        <div className="absolute translate-x-1/2 max-w-[575px]">
           <Orb />
         </div>
         <div className="flex-1 flex mt-[220px] min-h-[500px]">
